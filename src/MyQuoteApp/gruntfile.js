785183e11670454bbdaeda29fb7ca061a3b10d03
Wesley Xavier <wesleyx.oliveira@benner.com.br>
2016-01-08 14:31:20 -0200

    Asp.net 5 - Aula01

---
+++ src/MyQuoteApp/gruntfile.js
@@ -0,0 +1,14 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+
+    //Iniciar Dependencias
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.initConfig({
+
+    });
+};
